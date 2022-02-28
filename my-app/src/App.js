366bc9fa157808a81df4e0d34ac606735bcd31eb
Hugo Tran <006.tran.h@gmail.com>
2022-02-27 17:32:54 -0800

    Creating React App

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
